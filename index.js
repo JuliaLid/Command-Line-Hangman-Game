638360ff32404170bd623c227ac54c0029cc3978
Julia Lidstrom <juleslidstrom@gmail.com>
2018-02-12 11:00:10 -0600

    Working validator for input,  function to restart the game

--- index.js
+++ index.js
@@ -5,19 +5,7 @@ var prompt = require('prompt');
 var colors = require('colors');
 
 //global variables
-
 var newWord;
-
-
-// var schema = {
-//     properties: {
-//       name: {
-//         pattern: /^[a-zA-Z]+$/,
-//         message: 'Guess must a letters',
-//         required: true
-//       },
-//     }
-//   };
 
 //Welcome messages
 console.log(colors.magenta.bold("Welcome to Hangman!"));
@@ -40,16 +28,16 @@ function startGame(){
 function gamePlay(){
     prompt.start();
     prompt.message = colors.green.bold("?");
-    prompt.get({
-    properties: {
-        name: {
-        description: colors.green.bold("Guess a letter")
-        }
-        // validator: /^[a-zA-Z]+$/,
-        // warning: "Must provide a letter"
-    }
-    }, function (err, result) {
-        var guessedLetter = (result.name).toUpperCase();//capture guessed letter
+
+    var property = {
+        name: 'input',
+        message:colors.green.bold('Guess a letter'),
+        validator: /^(a|A)/,
+        warning: colors.red.bold('Must be a single letter'),
+    };
+
+    prompt.get(property, function (err, result) {
+        var guessedLetter = (result.input).toUpperCase();//capture guessed letter
         newWord.guess(guessedLetter); //calling guess method in Word function 
         
         newWord.wordIsComplete(); //checking if the word has been guessed
@@ -62,15 +50,35 @@ function gamePlay(){
             gamePlay();
         } else if (newWord.guessedWord === true){
             console.log(colors.cyan.bold("Way to go! You know your Pixar characters!"));
-            startGame();
+            resetGame();
         } else {
             console.log(colors.grey.bold("Sorry, dude. You're out of guesses! Better luck next time."));
+            resetGame();
+        }
+    });
+};
+
+
+function resetGame(){
+    prompt.start();
+    prompt.message = colors.green.bold("?");
+    var property = {
+        name: 'yesno',
+        message:colors.green.bold('Would you like to play again?'),
+        validator: /yes*|no]?/,
+        warning: colors.red.bold('Must respond yes or no'),
+       
+      };
+   
+    prompt.get(property,function (err, result) {
+        
+        if(result.yesno==="yes"){
             startGame();
+        } else {
+            console.log("Thanks");
         }
     });
 }
 
 //start game
 startGame();
-
-
